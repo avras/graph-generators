7ebef9d4488f5853d3b49ee1595266c9adfad09f
Saravanan Vijayakumaran <sarva.v@gmail.com>
2018-09-04 16:37:28 +0530

    Changes to Watts-Strogatz random graph generation algorithm

--- Data/Graph/Generators/Random/WattsStrogatz.hs
+++ Data/Graph/Generators/Random/WattsStrogatz.hs
@@ -1,15 +1,15 @@
 {-|
-  Implementations of binomially random graphs, as described by Erdős and Rényi.
+  Implementations of random graphs with small-world properties, as described
+  by Watts and Strogatz
 
-  Graphs generated using this method have a constant edge probability between two nodes.
-
-  See Erdős and A. Rényi, On Random Graphs, Publ. Math. 6, 290 (1959).
+  See Watts, D.J. and Strogatz, S.H.. Collective dynamics of `small-world'
+  networks, Nature, 393, pp 440-442, (1998)
 
   graph-generators copyright:
-    Copyright (C) 2014 Uli Köhler
+    Copyright (C) 2014-2018 Uli Köhler
 
   NetworkX copyright:
-    Copyright (C) 2004-2010 by 
+    Copyright (C) 2004-2018 by 
     Aric Hagberg <hagberg@lanl.gov>
     Dan Schult <dschult@colgate.edu>
     Pieter Swart <swart@lanl.gov>
@@ -20,10 +20,6 @@ module Data.Graph.Generators.Random.WattsStrogatz (
   -- ** Graph generators
         wattsStrogatzGraph,
         wattsStrogatzGraph',
-        -- ** Graph component generators
-        wattsStrogatzContext,
-        -- ** Utility functions
-        selectWithProbability
     )
     where
 
@@ -33,29 +29,6 @@ import Data.Graph.Generators
 import Control.Applicative ((<$>))
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-
-{-|
-    Generate a small-world context using the Wattz Strogatz method.
-
-    See 'wattsStrogatzGraph' for a detailed algorithm description.
-
-    Example usage, using a truly random generator:
-    
-    > import System.Random.MWC
-    > gen <- withSystemRandom . asGenIO $ return
-    > 
--}
-wattsStrogatzContext :: GenIO  -- ^ The random number generator to use
-           -> Int     -- ^ Identifier of the context's central node
-           -> [Int]   -- ^ The algorithm will generate random edges to those nodes
-                      --   from or to the given node
-           -> Double  -- ^ The probability for any pair of nodes to be connected
-           -> IO GraphContext -- ^ The resulting graph (IO required for randomness)
-wattsStrogatzContext gen n allNodes p = do
-    let endpoints = selectWithProbability gen p allNodes
-    inEdges <- endpoints
-    outEdges <- endpoints
-    return $ GraphContext inEdges n outEdges
 
 {-|
     Generate a unlabelled undirected random graph using the Algorithm introduced by
@@ -74,6 +47,7 @@ wattsStrogatzContext gen n allNodes p = do
     > wattsStrogatzGraph gen 1000 10 0.6
     ...
     
+    Modelled after NetworkX 2.1 watts_strogatz_graph().
 -}
 wattsStrogatzGraph :: GenIO  -- ^ The random number generator to use
            -> Int    -- ^ n, The number of nodes
@@ -82,54 +56,28 @@ wattsStrogatzGraph :: GenIO  -- ^ The random number generator to use
            -> IO GraphInfo -- ^ The resulting graph (IO required for randomness)
 wattsStrogatzGraph gen n k p = do
   let allNodes = [0..n-1]
-  -- Outgoing edge targets for any node
-  let insert m (i, js) = Map.insert i js m
-  let initialEdges = foldl (insert) (Map.empty) [ (i, forward_neighbors i) | i <- allNodes ]
-  allEdges <- rewrites (return initialEdges) $
-              [ (i, j) |
-                (i,s) <- Map.toList initialEdges,
-                j <- Set.toList s ]
-  return $ GraphInfo n (delineate allEdges)
+      k' = k `div` 2
+      edgesInRewriteOrder = concat $ fmap forwardEdgeList [1..k'] where
+        forwardEdgeList i = zip allNodes $ fmap (\j -> j+i `mod` n) allNodes
+      initialEdgeSet = Set.fromList edgesInRewriteOrder
+  finalEdgeSet <- rewireAllEdges initialEdgeSet edgesInRewriteOrder
+  return $ GraphInfo n (Set.toList finalEdgeSet)
   where
-    k' = fromInteger.toInteger $ k
-    forward_neighbors :: Int -> Set.Set Int 
-    forward_neighbors i = foldr (Set.insert) Set.empty $
-                          fmap (`mod` n)
-                          [i+1..i+k']
-    rewrites :: IO (Map.Map Int (Set.Set Int))
-             -> [(Int, Int)]
-             -> IO (Map.Map Int (Set.Set Int))
-    rewrites ioedges [] = ioedges
-    rewrites ioedges (t:tuples) = do
+    rewireAllEdges :: Set.Set (Int, Int) -> [(Int, Int)] -> IO (Set.Set (Int, Int))
+    rewireAllEdges edgeSet [] = return edgeSet
+    rewireAllEdges edgeSet (t:tuples) = do
       r <- uniform gen :: IO Double
-      edges <- ioedges :: IO (Map.Map Int (Set.Set Int))
       if (r > p)
-        then rewrites (return edges) tuples
-        else do es <- rewrite t edges
-                rewrites (return es) tuples
-    rewrite (i, j1) edges = do
-      r <- uniform gen :: IO Double
-      let j2 = floor $ r*((fromInteger.toInteger) n)
-      if (((member (i, j2) edges) || (member (j2, i) edges)) || (i == j2)) 
-        then rewrite (i, j1) edges
-        else return $ swap (i, j1) (i, j2) edges
+        then rewireAllEdges edgeSet tuples
+        else do es <- rewireEdge t edgeSet
+                rewireAllEdges es tuples
 
-delineate :: Map.Map a (Set.Set b) -> [(a, b)]
-delineate m = [ (i, j) |
-                (i, js) <- Map.toList m,
-                j <- Set.toList js ]
-
-member :: (Int, Int) -> Map.Map Int (Set.Set Int) -> Bool
-member (i, j) m = Set.member j (Map.findWithDefault Set.empty i m)
-
-swap :: (Ord a, Ord b)
-        => (a, b) -> (a, b)
-        -> Map.Map a (Set.Set b)
-        -> Map.Map a (Set.Set b)
-swap (k1, v1) (k2, v2) m =
-  let set1 = Map.findWithDefault Set.empty k1 m in
-  let set2 = Set.insert v2 $ Set.delete v1 set1 in
-  let m2 = Map.insert k2 set2 $ Map.delete k1 m in m2
+    rewireEdge :: (Int, Int) -> Set.Set (Int, Int) -> IO (Set.Set (Int, Int))
+    rewireEdge (i, j) edgeSet = do
+      j' <- uniformR (0, n-1) gen
+      if (((Set.member (i, j') edgeSet) || (Set.member (j', i) edgeSet)) || (i == j')) 
+        then rewireEdge (i, j) edgeSet
+        else return $ Set.insert (i, j') $ Set.delete (i, j) edgeSet
 
 {-|
     Like 'wattsStrogatzGraph', but uses a newly initialized random number generator.
@@ -150,13 +98,3 @@ wattsStrogatzGraph' :: Int    -- ^ n, The number of nodes
                  -> IO GraphInfo -- ^ The resulting graph (IO required for randomness)
 wattsStrogatzGraph' n k p =
     withSystemRandom . asGenIO $ \gen -> wattsStrogatzGraph gen n k p
-
-selectWithProbability :: GenIO  -- ^ The random generator state
-                      -> Double -- ^ The probability to select each list element
-                      -> [a]    -- ^ The list to filter
-                      -> IO [a] -- ^ The filtered list  
-selectWithProbability _   _ [] = return []
-selectWithProbability gen p (x:xs) = do
-    r <- uniform gen :: IO Double
-    let v = [ x | r <= p ]
-    liftM2 (++) (return v) $ selectWithProbability gen p xs
